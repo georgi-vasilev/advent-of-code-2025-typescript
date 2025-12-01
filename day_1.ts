bb53b97c6a501a43cc0c9d254b86fc36bc3da978
Georgi Vasilev <georgisvasilevv@gmail.com>
2025-12-01 11:29:36 +0200

    feat: solution for day 1 in typescript. remove unnecessary map

--- day_1.ts
+++ day_1.ts
@@ -1,33 +1,26 @@
 const text = await Deno.readTextFile("input_day_1");
 const inputs: Array<string> = text.split("\n").map((line: string) => line.trim());
 
-
 let dial: number = 50;
-const map = new Map<number, number>();
+let passes: number = 0;
 
 for (const input of inputs) {
   const rotations = Number(input.substring(1));
+  const direction = input.startsWith("L") ? -1 : 1;
 
-  const delta: number = input.startsWith("L") ? -rotations : rotations;
-
-  dial = mod(dial + delta, 100);
-
-  map.set(dial, (map.get(dial) || 0) + 1);
-}
-
-let maxKey: number = 0;
-let maxValue: number = -Infinity;
-
-for (const [key, value] of map.entries()) {
-  if (value > maxValue) {
-    maxValue = value;
-    maxKey = key;
+  let distToZero: number;
+  if (direction === 1) {
+    distToZero = dial === 0 ? 100 : (100 - dial);
+  } else {
+    distToZero = dial === 0 ? 100 : dial;
   }
+
+  // Count how many times we hit 0
+  if (rotations >= distToZero) {
+    passes += 1 + Math.floor((rotations - distToZero) / 100);
+  }
+
+  dial = ((dial + direction * rotations) % 100 + 100) % 100;
 }
 
-console.log(maxValue);
-
-function mod(number: number, modulo: number): number {
-  return ((number % modulo) + modulo) % modulo;
-}
-
+console.log(passes);
