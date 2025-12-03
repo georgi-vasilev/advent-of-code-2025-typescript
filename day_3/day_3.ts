d90cf42600f2ec419417dfa3799a77e314a2fb93
Georgi Vasilev <georgisvasilevv@gmail.com>
2025-12-03 10:10:43 +0200

    feat: day 2 part 2

--- day_3/day_3.ts
+++ day_3/day_3.ts
@@ -2,16 +2,8 @@ const input: string = await Deno.readTextFile("input_day_3");
 const joltage = input.split("\n").map((line: string) => line.trim());
 //const joltage: string[] = ["987654321111111", "811111111111119", "234234234234278", "818181911112111"];
 
-
-//In 987654321111111, you can make the largest joltage possible, 98, by turning on the first two batteries.
-//In 811111111111119, you can make the largest joltage possible by turning on the batteries labeled 8 and 9, producing 89 jolts.
-//In 234234234234278, you can make 78 by turning on the last two batteries (marked 7 and 8).
-//In 818181911112111, the largest joltage you can produce is 92.
-//The total output joltage is the sum of the maximum joltage from each bank, so in this example, the total output joltage is 98 + 89 + 78 + 92 = 357.
-
-let counter = 0;
 const result = joltage.reduce((acc: number, curr: string) => {
-  const joltage = findBiggestJoltage(curr);
+  const joltage = findBiggest12DigitJoltage(curr);
   acc += joltage;
 
   return acc;
@@ -19,6 +11,7 @@ const result = joltage.reduce((acc: number, curr: string) => {
 
 console.log(result);
 
+// part 1 function
 function findBiggestJoltage(str: string): number {
   let maxPair = -1;
   let bestToTheRight = -1;
@@ -34,4 +27,24 @@ function findBiggestJoltage(str: string): number {
 
 
   return maxPair === -1 ? 0 : maxPair;
+}
+
+// part 2 funcs
+function maxNDigits(str: string, keep: number): string {
+  let toRemove: number = str.length - keep;
+  const stack: string[] = [];
+
+  for (const ch of str) {
+    while (toRemove > 0 && stack.length && stack[stack.length - 1] < ch) {
+      stack.pop();
+      toRemove--;
+    }
+    stack.push(ch);
+  }
+
+  return stack.slice(0, keep).join('');
+}
+
+function findBiggest12DigitJoltage(str: string): number {
+  return Number(maxNDigits(str, 12));
 }
