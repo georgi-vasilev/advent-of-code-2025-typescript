e1237ec6d0a575d055e33f8b9c090bf62eb5ae3e
Georgi Vasilev <georgisvasilevv@gmail.com>
2025-12-05 11:19:41 +0200

    feat: day 5 part 2. todo: optimize part 1

--- day_5/day_5.ts
+++ day_5/day_5.ts
@@ -1,21 +1,54 @@
 const text: string = await Deno.readTextFile("input_day_5");
 const [rangeBlock, numberBlock] = text.trim().split(/\n\s*\n/);
 
-const ranges: Array<Array<number>> = rangeBlock.split("\n").map((line: string) => line.split("-").map(Number));
+const ranges: Array<[number, number]> = rangeBlock.split("\n").map((line: string) => line.split("-").map(Number));
 const numbers: Array<number> = numberBlock.split("\n").map(Number);
 
 
-const result: Set<number> = new Set<number>();
-
 // could be optimized by merging the ranges where possible but eh
-for (const [start, end] of ranges) {
-  for (const num of numbers) {
-    if (start <= num && num <= end) {
-      if (!result.has(num)) {
-        result.add(num);
+function findFreshIngredients(set: Set<number>, ranges: Array<[number, number]>): number {
+  for (const [start, end] of ranges) {
+    for (const num of numbers) {
+      if (start <= num && num <= end) {
+        if (!set.has(num)) {
+          set.add(num);
+        }
       }
     }
+
   }
+  return set.size;
 }
 
-console.log(result.size);
+function mergeRanges(ranges: Array<[number, number]>): Array<[number, number]> {
+  ranges.sort((a, b) => a[0] - b[0]);
+
+  const merged: Array<[number, number]> = [];
+  let [start, end] = ranges[0];
+
+  for (let i = 1; i < ranges.length; i++) {
+    const [s, e] = ranges[i];
+
+    if (s <= end) {
+      end = Math.max(end, e);
+    } else {
+      merged.push([start, end]);
+      [start, end] = [s, e];
+    }
+  }
+
+  merged.push([start, end]);
+  return merged;
+}
+
+function totalCovered(merged: Array<[number, number]>): number {
+  let total = 0;
+  for (const [start, end] of merged) {
+    total += (end - start + 1);
+  }
+  return total;
+}
+
+const mergedRanges: Array<[number, number]> = mergeRanges(ranges);
+
+console.log(totalCovered(mergedRanges));
