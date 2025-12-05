81c54b7c14cf460d35e09cb11e27910b1f058818
Georgi Vasilev <georgisvasilevv@gmail.com>
2025-12-05 10:52:43 +0200

    feat: day 5 part 1

---
+++ day_5/day_5.ts
@@ -0,0 +1,20 @@
+const text: string = await Deno.readTextFile("input_day_5");
+const [rangeBlock, numberBlock] = text.trim().split(/\n\s*\n/);
+
+const ranges: Array<Array<number>> = rangeBlock.split("\n").map((line: string) => line.split("-").map(Number));
+const numbers: Array<number> = numberBlock.split("\n").map(Number);
+
+
+const result: Map<number, number> = new Map<number, number>();
+
+for (const [start, end] of ranges) {
+  for (const num of numbers) {
+    if (start <= num && num <= end) {
+      if (!result.has(num)) {
+        result.set(num, num);
+      }
+    }
+  }
+}
+
+console.log([...result.values()].length);
