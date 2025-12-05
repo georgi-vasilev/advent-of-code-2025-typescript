7ec50182a1277e111d89281e24c7ca32071d1188
Georgi Vasilev <georgisvasilevv@gmail.com>
2025-12-05 10:52:35 +0200

    feat: day 4 part 2

--- day_4/day_4.ts
+++ day_4/day_4.ts
@@ -1,17 +1,17 @@
 const text: string = await Deno.readTextFile("input_day_4");
 const inputs: Array<Array<string>> = text.split("\n").map((line: string) => line.trim().split(""));
 
-console.log(dfs(inputs));
+console.log(forklift(inputs));
 
 
 
 
 // part 1 func
 function dfs(adjencency: Array<Array<string>>): number {
-  const rowLength = adjencency.length;
-  const colLength = adjencency[0].length;
+  const rowLength: number = adjencency.length;
+  const colLength: number = adjencency[0].length;
 
-  const dirs = [
+  const dirs: Array<Array<number>> = [
     [-1, -1],
     [-1, 0],
     [-1, 1],
@@ -27,9 +27,9 @@ function dfs(adjencency: Array<Array<string>>): number {
   let availableScrolls: number = 0;
 
   const stack: Stack<number> = [];
-  for (let sr = 0; sr < rowLength; sr++) {
+  for (let sr: number = 0; sr < rowLength; sr++) {
 
-    for (let sc = 0; sc < colLength; sc++) {
+    for (let sc: number = 0; sc < colLength; sc++) {
       if (adjencency[sr][sc] !== '@' || visited[sr][sc]) {
         continue;
       }
@@ -37,17 +37,17 @@ function dfs(adjencency: Array<Array<string>>): number {
       stack.push([sr, sc]);
 
       while (stack.length > 0) {
-        const [row, col] = stack.pop()!;
+        const [row, col]: [number, number] = stack.pop()!;
         if (visited[row][col]) {
           continue;
         }
 
         visited[row][col] = true;
 
-        let neighbourCount = 0;
+        let neighbourCount: number = 0;
         for (const [dr, dc] of dirs) {
-          const nr = row + dr;
-          const nc = col + dc;
+          const nr: number = row + dr;
+          const nc: number = col + dc;
 
           if (nr < 0 || nc < 0 || nr >= rowLength || nc >= colLength) {
             continue;
@@ -67,6 +67,81 @@ function dfs(adjencency: Array<Array<string>>): number {
   }
 
   return availableScrolls;
+
+}
+
+// part 2 function
+function findRemovable(adjencency: Array<Array<string>>): Array<Array<number>> {
+  const rowLength: number = adjencency.length;
+  const colLength: number = adjencency[0].length;
+
+  const dirs: Array<Array<number>> = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+  ];
+
+  const visited = Array.from({ length: rowLength }, () => Array(colLength).fill(false));
+  const removable: Array<[number, number]> = [];
+
+  for (let sr: number = 0; sr < rowLength; sr++) {
+    for (let sc: number = 0; sc < colLength; sc++) {
+      if (adjencency[sr][sc] !== '@' || visited[sr][sc]) continue;
+
+      const stack: Array<[number, number]> = [[sr, sc]];
+
+      while (stack.length > 0) {
+        const [r, c] = stack.pop()!;
+        if (visited[r][c]) continue;
+        visited[r][c] = true;
+
+        let neighbours = 0;
+
+        for (const [dr, dc] of dirs) {
+          const nr = r + dr, nc = c + dc;
+
+          if (nr < 0 || nc < 0 || nr >= rowLength || nc >= colLength) {
+            continue;
+          }
+
+          if (adjencency[nr][nc] === '@') {
+            neighbours++;
+            if (!visited[nr][nc]) stack.push([nr, nc]);
+          }
+        }
+
+        // removable rule
+        if (neighbours < 4) {
+          removable.push([r, c]);
+        }
+      }
+    }
+  }
+
+  return removable;
+}
+
+function forklift(adjencency: Array<Array<string>>): number {
+  let totalRemoved: number = 0;
+
+  while (true) {
+    const removable = findRemovable(adjencency);
+    if (removable.length === 0) {
+      break;
+    }
+    for (const [row, col] of removable) {
+      adjencency[row][col] = '.';
+    }
+
+    totalRemoved += removable.length;
+  }
+
+  return totalRemoved;
 
 }
 
